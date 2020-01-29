39f33d650415e8296880d05cdc44df4b3532b31d
Dev.Mook <jeonginmook@gmail.com>
2020-01-29 16:03:17 +0900

    Modify API Request Mapping URL
    Backward compatibility considerations

--- src/main/java/com/example/template/ProductController.java
+++ src/main/java/com/example/template/ProductController.java
@@ -9,7 +9,24 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
+    // 하위 호환성을 유지하기 위해 
+    // "/item/{productId}"와 "/product/{productId}" API 둘다 사용 가능하도록
+    // Source Code 수정
+    
     @GetMapping("/item/{productId}")
+    Product itemStockCheck(@PathVariable(value = "productId") Long productId) {
+
+        System.out.println("itemStockCheck call");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return  this.productService.getProductById(productId);
+    }
+    
+    @GetMapping("/product/{productId}")
     Product productStockCheck(@PathVariable(value = "productId") Long productId) {
 
         System.out.println("productStockCheck call");
